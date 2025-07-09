c3368f2a825ef58194f3fe04021fea46aa198bc7
Jiri Slaby <jirislaby@gmail.com>
2025-07-09 09:23:31 +0200

    tests: add ratelimit

---
+++ tests/test_ratelimit.cpp
@@ -0,0 +1,23 @@
+#include <cassert>
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+#include "helpers/Ratelimit.h"
+
+int main()
+{
+	SlHelpers::Ratelimit r(std::chrono::milliseconds(100));
+	auto counter = 0U;
+
+	for (auto i = 0U; i < 99U; ++i) {
+		if (r.limit())
+			counter++;
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	}
+
+	std::cout << "counter=" << counter << '\n';
+	assert(counter == 10);
+
+	return 0;
+}
