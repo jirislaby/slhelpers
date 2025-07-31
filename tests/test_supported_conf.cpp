a12aa15f62e489053fb55e81d07deab1e7ac894e
Jiri Slaby <jirislaby@gmail.com>
2025-07-31 08:49:17 +0200

    tests: clean up test_supported_conf

--- tests/test_supported_conf.cpp
+++ tests/test_supported_conf.cpp
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
 #include <cassert>
-#include <iostream>
 #include <string>
 
 #include "kerncvs/SupportedConf.h"
@@ -24,15 +23,15 @@ int main()
 	};
 
 	const SupportedConf supp { supportedConf };
-	/*for (const auto &e : map)
-		std::cout << "E=" << e.first << " -> " << e.second << '\n';*/
 
 	assert(supp.supportState("non_existing") == SupportedConf::NonPresent);
 
-	assert(supp.supportState("drivers/input/joystick/something.ko") == SupportedConf::Unsupported);
+	assert(supp.supportState("drivers/input/joystick/something.ko") ==
+	       SupportedConf::Unsupported);
 	assert(supp.supportState("drivers/mtd/spi-nor/spi-nor") == SupportedConf::Unspecified);
 	assert(supp.supportState("drivers/mtd/something_else") == SupportedConf::Unsupported);
-	assert(supp.supportState("drivers/usb/typec/altmodes/typec_displayport") == SupportedConf::Unsupported);
+	assert(supp.supportState("drivers/usb/typec/altmodes/typec_displayport") ==
+	       SupportedConf::Unsupported);
 	assert(supp.supportState("fs/ocfs2/ocfs2_stackglue") == SupportedConf::KMPSupported);
 	assert(supp.supportState("drivers/net/ethernet/8390/8390") == SupportedConf::BaseSupported);
 	assert(supp.supportState("drivers/ata/ahci_imx") == SupportedConf::UnsupportedOptional);
