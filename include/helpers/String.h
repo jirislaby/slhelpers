c2eb1e64a0410d7f58e759e085b88237e3327ed4
Jiri Slaby <jirislaby@gmail.com>
2025-12-11 11:26:11 +0100

    helpers/String: use string_view for startsWith(), endsWith()
    string_view is (cheaply) constructible from string, not vice versa.

--- include/helpers/String.h
+++ include/helpers/String.h
@@ -8,7 +8,6 @@
 #include <cctype>
 #include <optional>
 #include <string>
-#include <utility>
 #include <vector>
 
 namespace SlHelpers {
@@ -17,11 +16,11 @@ class String {
 public:
 	String() = delete;
 
-	static bool startsWith(const std::string &what, const std::string &startsWith) {
+	static bool startsWith(const std::string_view &what, const std::string_view &startsWith) {
 		return !what.compare(0, startsWith.length(), startsWith);
 	}
 
-	static bool endsWith(const std::string &what, const std::string &endsWith) {
+	static bool endsWith(const std::string_view &what, const std::string_view &endsWith) {
 		auto wlen = what.length();
 		auto elen = endsWith.length();
 		return wlen >= elen && !what.compare(wlen - elen, std::string::npos, endsWith);
