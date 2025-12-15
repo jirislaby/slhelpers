jirislaby/slhelpers

commit f2c7659d782050d6d6bad708cf444e4ef22c97e9

sqlite: propagate errors from reset
According to docs, step can succeed, but only the reset later reports
an error.

include/sqlite/SQLConn.h
@@ -159,6 +159,7 @@ protected:
 	mutable int m_lastErrorCodeExt;
 private:
 	static int busyHandler(void *, int count);
+	void dumpBinding(const Binding &binding) const noexcept;
 };
 
 class Select {

src/sqlite/SQLConn.cpp
@@ -284,17 +284,8 @@ bool SQLConn::step(const SQLStmtHolder &ins, uint64_t *affected) const noexcept
 	return false;
 }
 
-bool SQLConn::insert(const SQLStmtHolder &ins, const Binding &binding,
-		     uint64_t *affected) const noexcept
+void SQLConn::dumpBinding(const Binding &binding) const noexcept
 {
-	SQLStmtResetter insResetter(sqlHolder, ins);
-
-	if (!bind(ins, binding))
-		return false;
-
-	if (step(ins, affected))
-		return true;
-
 	for (const auto &b : binding) {
 		m_lastError << '\t' << b.first << '=';
 		if (std::holds_alternative<int>(b.second))
@@ -308,8 +299,29 @@ bool SQLConn::insert(const SQLStmtHolder &ins, const Binding &binding,
 		else
 			m_lastError << "NULL";
 	}
+}
 
-	return false;
+bool SQLConn::insert(const SQLStmtHolder &ins, const Binding &binding,
+		     uint64_t *affected) const noexcept
+{
+	SQLStmtResetter insResetter(sqlHolder, ins);
+
+	if (!bind(ins, binding))
+		return false;
+
+	if (!step(ins, affected)) {
+		dumpBinding(binding);
+		return false;
+	}
+
+	auto ret = insResetter.reset();
+	if (ret != SQLITE_OK) {
+		setError(ret, "db stmt reset (INSERT) failed", true);
+		dumpBinding(binding);
+		return false;
+	}
+
+	return true;
 }
 
 std::optional<SQLConn::SelectResult>

include/sqlite/SQLiteSmart.h
@@ -16,6 +16,8 @@ using SQLStmtHolder = SlHelpers::UniqueHolder<sqlite3_stmt>;
 struct SQLStmtResetter {
 	SQLStmtResetter(sqlite3 *sql, sqlite3_stmt *stmt) : sql(sql), stmt(stmt) { }
 	~SQLStmtResetter();
+
+	int reset();
 private:
 	sqlite3 *sql;
 	sqlite3_stmt *stmt;

src/sqlite/SQLiteSmart.cpp
@@ -21,10 +21,17 @@ void SlHelpers::Deleter<sqlite3_stmt>::operator()(sqlite3_stmt *stmt) const
 
 SQLStmtResetter::~SQLStmtResetter()
 {
-	int ret = sqlite3_reset(stmt);
+	auto ret = reset();
 	if (ret != SQLITE_OK && ret != SQLITE_CONSTRAINT) {
 		std::cerr << "stmt reset failed (" << __LINE__ << "): " <<
 			     sqlite3_errstr(ret) << " -> " <<
 			     sqlite3_errmsg(sql) << "\n";
 	}
+}
+
+int SQLStmtResetter::reset()
+{
+	auto ret = sqlite3_reset(stmt);
+	stmt = nullptr;
+	return ret;
 }

commit 363b9124ba3f7bf0d23dec857cf09d899070aba0

sqlite: do not report UNIQUE errors from reset
After commit f2c7659d7820, UNIQUE errors would be reported even when
they are not called for.

Fixes: f2c7659d7820 ("sqlite: propagate errors from reset")

include/sqlite/SQLConn.h
@@ -137,7 +137,8 @@ protected:
 		  const BindVal &val, bool transient = false) const noexcept;
 	bool bind(const SQLStmtHolder &ins, const Binding &binding,
 		  bool transient = false) const noexcept;
-	bool step(const SQLStmtHolder &ins, uint64_t *affected = nullptr) const noexcept;
+	bool step(const SQLStmtHolder &ins, uint64_t *affected = nullptr,
+		  bool *uniqueError = nullptr) const noexcept;
 	bool insert(const SQLStmtHolder &ins, const Binding &binding = {},
 		    uint64_t *affected = nullptr) const noexcept;
 

src/sqlite/SQLConn.cpp
@@ -263,23 +263,28 @@ bool SQLConn::bind(const SQLStmtHolder &ins, const Binding &binding, bool transi
 	return true;
 }
 
-bool SQLConn::step(const SQLStmtHolder &ins, uint64_t *affected) const noexcept
+bool SQLConn::step(const SQLStmtHolder &ins, uint64_t *affected, bool *uniqueError) const noexcept
 {
 	auto ret = sqlite3_step(ins);
 	if (ret == SQLITE_DONE) {
 		if (affected)
 			*affected = sqlite3_changes64(sqlHolder);
+		if (uniqueError)
+			*uniqueError = false;
 		return true;
 	}
 
-	setError(ret, "db step (INSERT) failed", true);
+	const auto uniqueErrorLoc = sqlite3_extended_errcode(sqlHolder) == SQLITE_CONSTRAINT_UNIQUE;
+	if (uniqueError)
+		*uniqueError = uniqueErrorLoc;
 
-	if (m_lastErrorCodeExt == SQLITE_CONSTRAINT_UNIQUE &&
-			!(m_flags & OpenFlags::ERROR_ON_UNIQUE_CONSTRAINT)) {
+	if (uniqueErrorLoc && !(m_flags & OpenFlags::ERROR_ON_UNIQUE_CONSTRAINT)) {
 		if (affected)
 			*affected = 0;
 		return true;
 	}
+
+	setError(ret, "db step (INSERT) failed", true);
 
 	return false;
 }
@@ -309,13 +314,14 @@ bool SQLConn::insert(const SQLStmtHolder &ins, const Binding &binding,
 	if (!bind(ins, binding))
 		return false;
 
-	if (!step(ins, affected)) {
+	bool uniqueError;
+	if (!step(ins, affected, &uniqueError)) {
 		dumpBinding(binding);
 		return false;
 	}
 
 	auto ret = insResetter.reset();
-	if (ret != SQLITE_OK) {
+	if (!uniqueError && ret != SQLITE_OK) {
 		setError(ret, "db stmt reset (INSERT) failed", true);
 		dumpBinding(binding);
 		return false;