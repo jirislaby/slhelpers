jirislaby/slhelpers

commit 67160afa1f06167ba4c5f0d2c9858a71c829751b

git: convert to LastError
This means switch most int retvals to bool. This is to unification with
the rest of the libraries.

src/git/Repo.cpp
@@ -23,6 +23,8 @@ void SlHelpers::Deleter<git_repository>::operator()(git_repository *repo) const
 	git_repository_free(repo);
 }
 
+thread_local SlHelpers::LastErrorStr<int, int> Repo::m_lastError;
+
 std::optional<Repo> Repo::init(const std::filesystem::path &path, bool bare,
 				 const std::string &originUrl) noexcept
 {
@@ -69,11 +71,13 @@ bool Repo::update(const std::filesystem::path &path, const std::string &remote)
 	return true;
 }
 
-int Repo::checkoutTree(const Tree &tree, unsigned int strategy) const noexcept
+bool Repo::checkoutTree(const Tree &tree, unsigned int strategy) const noexcept
 {
 	git_checkout_options opts GIT_CHECKOUT_OPTIONS_INIT;
 	opts.checkout_strategy = strategy;
-	return git_checkout_tree(repo(), reinterpret_cast<const git_object *>(tree.tree()), &opts);
+	return !setLastError(git_checkout_tree(repo(),
+					       reinterpret_cast<const git_object *>(tree.tree()),
+					       &opts));
 }
 
 std::optional<std::string> Repo::catFile(const std::string &branch,
@@ -88,7 +92,7 @@ std::optional<std::string> Repo::catFile(const std::string &branch,
 std::optional<Blob> Repo::blobCreateFromWorkDir(const std::filesystem::path &file) const noexcept
 {
 	git_oid oid;
-	if (git_blob_create_from_workdir(&oid, repo(), file.c_str()))
+	if (setLastError(git_blob_create_from_workdir(&oid, repo(), file.c_str())))
 		return std::nullopt;
 	return blobLookup(oid);
 }
@@ -96,7 +100,7 @@ std::optional<Blob> Repo::blobCreateFromWorkDir(const std::filesystem::path &fil
 std::optional<Blob> Repo::blobCreateFromDisk(const std::filesystem::path &file) const noexcept
 {
 	git_oid oid;
-	if (git_blob_create_from_disk(&oid, repo(), file.c_str()))
+	if (setLastError(git_blob_create_from_disk(&oid, repo(), file.c_str())))
 		return std::nullopt;
 	return blobLookup(oid);
 }
@@ -104,7 +108,7 @@ std::optional<Blob> Repo::blobCreateFromDisk(const std::filesystem::path &file) 
 std::optional<Blob> Repo::blobCreateFromBuffer(const std::string &buf) const noexcept
 {
 	git_oid oid;
-	if (git_blob_create_from_buffer(&oid, repo(), buf.c_str(), buf.length()))
+	if (setLastError(git_blob_create_from_buffer(&oid, repo(), buf.c_str(), buf.length())))
 		return std::nullopt;
 	return blobLookup(oid);
 }
@@ -142,8 +146,9 @@ std::optional<Commit> Repo::commitCreate(const Signature &author, const Signatur
 	for (const auto &p : parents)
 		parentPtrs.push_back(p->commit());
 
-	if (git_commit_create(&oid, repo(), "HEAD", author, committer, "UTF-8", msg.c_str(),
-			      tree, parentPtrs.size(), parentPtrs.data()))
+	if (setLastError(git_commit_create(&oid, repo(), "HEAD", author, committer, "UTF-8",
+					   msg.c_str(), tree, parentPtrs.size(),
+					   parentPtrs.data())))
 		return std::nullopt;
 	return commitLookup(oid);
 }
@@ -157,7 +162,7 @@ std::optional<Commit> Repo::commitCreateCheckout(const Signature &author,
 	auto commit = commitCreate(author, committer, msg, tree, parents);
 	if (!commit)
 		return std::nullopt;
-	if (checkoutTree(tree, strategy))
+	if (!checkoutTree(tree, strategy))
 		return std::nullopt;
 	return commit;
 }
@@ -231,7 +236,7 @@ std::variant<Blob, Commit, Tag, Tree, std::monostate>
 Repo::revparseSingle(const std::string &rev) const noexcept
 {
 	git_object *obj;
-	if (git_revparse_single(&obj, repo(), rev.c_str()))
+	if (setLastError(git_revparse_single(&obj, repo(), rev.c_str())))
 		return std::monostate{};
 
 	switch (git_object_type(obj)) {
@@ -293,8 +298,8 @@ std::optional<Tag> Repo::tagCreate(const std::string &tagName, const Object &tar
 				   bool force) const noexcept
 {
 	git_oid oid;
-	if (git_tag_create(&oid, repo(), tagName.c_str(), target.object(), tagger, message.c_str(),
-			   force))
+	if (setLastError(git_tag_create(&oid, repo(), tagName.c_str(), target.object(), tagger,
+					message.c_str(), force)))
 		return std::nullopt;
 	return tagLookup(oid);
 }
@@ -344,7 +349,7 @@ std::optional<TreeBuilder> Repo::treeBuilderCreate(const Tree *source) const noe
 	return MakeGit<TreeBuilder>(git_treebuilder_new, repo(), source ? source->tree() : nullptr);
 }
 
-std::pair<std::string, int> Repo::lastError() noexcept
+std::pair<std::string, int> Repo::lastGitError() noexcept
 {
 	const auto err = git_error_last();
 	return { err->message, err->klass };
@@ -381,32 +386,30 @@ std::optional<Repo> Repo::clone(const std::filesystem::path &path, const std::st
 	return MakeGit<Repo>(git_clone, url.c_str(), path.c_str(), &opts);
 }
 
-int Repo::checkout(const std::string &branch) const noexcept
+bool Repo::checkout(const std::string &branch) const noexcept
 {
 	auto ref = refLookup(branch);
 	if (!ref)
-		return -1;
+		return false;
 
 	return checkout(*ref);
 }
 
-int Repo::checkout(const Reference &ref) const noexcept
+bool Repo::checkout(const Reference &ref) const noexcept
 {
 	auto c = commitLookup(*ref.target());
 	if (!c)
-		return -1;
+		return false;
 
 	auto tree = c->tree();
 	if (!tree)
-		return -1;
+		return false;
 
-	auto ret = checkoutTree(*tree);
-	if (ret)
-		return ret;
+	if (!checkoutTree(*tree))
+		return false;
 
-	ret = git_repository_set_head(repo(), ref.name().c_str());
-	if (ret)
-		return ret;
+	if (setLastError(git_repository_set_head(repo(), ref.name().c_str())))
+		return false;
 
-	return 0;
+	return true;
 }

include/git/Diff.h
@@ -45,13 +45,24 @@ public:
 		return git_diff_get_delta(diff(), idx);
 	}
 
-	int findSimilar(const git_diff_find_options *options = nullptr) const noexcept {
-		return git_diff_find_similar(diff(), options);
+	bool findSimilar(const git_diff_find_options *options = nullptr) const noexcept {
+		return !git_diff_find_similar(diff(), options);
 	}
 
 	bool isSortedICase() const noexcept { return git_diff_is_sorted_icase(diff()); }
 
+	/**
+	 * @brief Invoke \p forEachCB callbacks for each file, hunk, ... in the diff
+	 * @param forEachCB Callbacks
+	 * @return 0 on success, non-zero callback return value, or error code.
+	 */
 	int forEach(const ForEachCB &forEachCB) const;
+	/**
+	 * @brief Invoke \p forEachCB callbacks for each file, hunk, ... in the diff
+	 * @param format \c GIT_DIFF_FORMAT_PATCH, \c GIT_DIFF_FORMAT_PATCH_HEADER, ...
+	 * @param printCB Callback
+	 * @return 0 on success, non-zero callback return value, or error code.
+	 */
 	int print(const git_diff_format_t &format, const PrintCB &printCB) const;
 
 	std::optional<Buf> toBuf(git_diff_format_t format) const noexcept;

include/git/Index.h
@@ -11,10 +11,10 @@
 #include <git2.h>
 
 #include "../helpers/Unique.h"
+#include "Repo.h"
 
 namespace SlGit {
 
-class Repo;
 class Tree;
 
 class IndexIterator {
@@ -73,9 +73,11 @@ public:
 	static std::optional<Index> open(const std::filesystem::path &path) noexcept;
 	static std::optional<Index> create() noexcept;
 
-	int read(bool force = true) const noexcept { return git_index_read(index(), force); }
-	int write() const noexcept { return git_index_write(index()); }
-	int readTree(const Tree &tree) const noexcept;
+	bool read(bool force = true) const noexcept {
+		return !Repo::setLastError(git_index_read(index(), force));
+	}
+	bool write() const noexcept { return !Repo::setLastError(git_index_write(index())); }
+	bool readTree(const Tree &tree) const noexcept;
 	std::optional<Tree> writeTree(const Repo &repo) const noexcept;
 
 	size_t entrycount() const noexcept { return git_index_entrycount(index()); }
@@ -87,15 +89,35 @@ public:
 		return git_index_get_bypath(index(), path.c_str(), stage);
 	}
 
-	int addByPath(const std::filesystem::path &path) const noexcept {
-		return git_index_add_bypath(index(), path.c_str());
+	bool addByPath(const std::filesystem::path &path) const noexcept {
+		return !Repo::setLastError(git_index_add_bypath(index(), path.c_str()));
 	}
-	int removeByPath(const std::filesystem::path &path) const noexcept {
-		return git_index_remove_bypath(index(), path.c_str());
+	bool removeByPath(const std::filesystem::path &path) const noexcept {
+		return !Repo::setLastError(git_index_remove_bypath(index(), path.c_str()));
 	}
+
+	/**
+	 * @brief Add all \p paths into this Index
+	 * @param paths Paths to add
+	 * @param flags Combination of git_index_add_option_t
+	 * @param cb Callback for each added/updated path (or nullptr)
+	 * @return 0 on success, negative callback return value, or error code.
+	 */
 	int addAll(const std::vector<std::string> &paths, unsigned int flags,
 		   const MatchCB *cb = nullptr) const;
+	/**
+	 * @brief Remove all \p paths from this Index
+	 * @param paths Paths to remove
+	 * @param cb Callback for each removed path (or nullptr)
+	 * @return 0 on success, negative callback return value, or error code.
+	 */
 	int removeAll(const std::vector<std::string> &paths, const MatchCB *cb = nullptr) const;
+	/**
+	 * @brief Update all \p paths in this Index
+	 * @param paths Paths to update
+	 * @param cb Callback for each updated path (or nullptr)
+	 * @return 0 on success, negative callback return value, or error code.
+	 */
 	int updateAll(const std::vector<std::string> &paths, const MatchCB *cb = nullptr) const;
 
 	bool hasConflicts() const noexcept { return git_index_has_conflicts(index()); }

include/git/Misc.h
@@ -8,6 +8,7 @@
 #include <git2.h>
 
 #include "../helpers/Unique.h"
+#include "Repo.h"
 
 namespace SlGit {
 
@@ -36,7 +37,6 @@ private:
 };
 
 class Commit;
-class Repo;
 
 class RevWalk {
 	using GitTy = git_revwalk;
@@ -46,28 +46,34 @@ class RevWalk {
 public:
 	RevWalk() = delete;
 
-	int push(const git_oid &oid) const noexcept { return git_revwalk_push(revWalk(), &oid); }
-	int push(const std::string &id) const noexcept;
-	int pushHead() const noexcept { return git_revwalk_push_head(revWalk()); }
-	int pushRef(const std::string &ref) const noexcept {
-		return git_revwalk_push_ref(revWalk(), ref.c_str());
+	bool push(const git_oid &oid) const noexcept {
+		return !Repo::setLastError(git_revwalk_push(revWalk(), &oid));
 	}
-	int pushGlob(const std::string &glob) const noexcept {
-		return git_revwalk_push_glob(revWalk(), glob.c_str());
+	bool push(const std::string &id) const noexcept;
+	bool pushHead() const noexcept {
+		return !Repo::setLastError(git_revwalk_push_head(revWalk()));
 	}
-	int pushRange(const std::string &range) const noexcept {
-		return git_revwalk_push_range(revWalk(), range.c_str());
+	bool pushRef(const std::string &ref) const noexcept {
+		return !Repo::setLastError(git_revwalk_push_ref(revWalk(), ref.c_str()));
+	}
+	bool pushGlob(const std::string &glob) const noexcept {
+		return !Repo::setLastError(git_revwalk_push_glob(revWalk(), glob.c_str()));
+	}
+	bool pushRange(const std::string &range) const noexcept {
+		return !Repo::setLastError(git_revwalk_push_range(revWalk(), range.c_str()));
 	}
 
 	/* Hiding marks stopping points */
-	int hide(const git_oid &oid) const noexcept { return git_revwalk_hide(revWalk(), &oid); }
-	int hide(const std::string &id) const noexcept;
-	int hideGlob(const std::string &glob) const noexcept {
-		return git_revwalk_hide_glob(revWalk(), glob.c_str());
+	bool hide(const git_oid &oid) const noexcept {
+		return !Repo::setLastError(git_revwalk_hide(revWalk(), &oid));
+	}
+	bool hide(const std::string &id) const noexcept;
+	bool hideGlob(const std::string &glob) const noexcept {
+		return !Repo::setLastError(git_revwalk_hide_glob(revWalk(), glob.c_str()));
 	}
 
-	int sorting(unsigned int mode) const noexcept {
-		return git_revwalk_sorting(revWalk(), mode);
+	bool sorting(unsigned int mode) const noexcept {
+		return !Repo::setLastError(git_revwalk_sorting(revWalk(), mode));
 	}
 
 	std::optional<Commit> next() const noexcept;

include/git/Remote.h
@@ -23,16 +23,16 @@ class Remote {
 public:
 	Remote() = delete;
 
-	int fetchRefspecs(FetchCallbacks &fc, const std::vector<std::string> &refspecs = {},
-			  int depth = 0, bool tags = true) const noexcept;
-	int fetchRefspecs(const std::vector<std::string> &refspecs = {}, int depth = 0,
-			  bool tags = true) const noexcept {
+	bool fetchRefspecs(FetchCallbacks &fc, const std::vector<std::string> &refspecs = {},
+			   int depth = 0, bool tags = true) const noexcept;
+	bool fetchRefspecs(const std::vector<std::string> &refspecs = {}, int depth = 0,
+			   bool tags = true) const noexcept {
 		DefaultFetchCallbacks fc;
 		return fetchRefspecs(fc, refspecs, depth, tags);
 	}
-	int fetchBranches(const std::vector<std::string> &branches, int depth = 0,
-			  bool tags = true) const noexcept;
-	int fetch(const std::string &branch, int depth = 0,
+	bool fetchBranches(const std::vector<std::string> &branches, int depth = 0,
+			   bool tags = true) const noexcept;
+	bool fetch(const std::string &branch, int depth = 0,
 		  bool tags = true) const noexcept { return fetchBranches({ branch }, depth, tags); }
 
 	std::string url() const noexcept { return git_remote_url(remote()); }

include/git/Repo.h
@@ -10,6 +10,7 @@
 
 #include <git2.h>
 
+#include "../helpers/LastError.h"
 #include "../helpers/Unique.h"
 
 #include "DefaultFetchCallbacks.h"
@@ -60,9 +61,9 @@ public:
 	 */
 	static bool update(const std::filesystem::path &path, const std::string &remote = "origin");
 
-	int checkout(const std::string &branch) const noexcept;
-	int checkout(const Reference &reference) const noexcept;
-	int checkoutTree(const Tree &tree, unsigned int strategy = GIT_CHECKOUT_SAFE) const noexcept;
+	bool checkout(const std::string &branch) const noexcept;
+	bool checkout(const Reference &reference) const noexcept;
+	bool checkoutTree(const Tree &tree, unsigned int strategy = GIT_CHECKOUT_SAFE) const noexcept;
 	std::optional<std::string> catFile(const std::string &branch,
 					   const std::string &file) const noexcept;
 
@@ -144,24 +145,46 @@ public:
 	std::filesystem::path path() const noexcept { return git_repository_path(repo()); }
 	std::filesystem::path workDir() const noexcept { return git_repository_workdir(repo()); }
 
-	static std::pair<std::string, int> lastError() noexcept;
+	/// @brief Return the last error string if some (from git_last_error())
+	static auto &lastError() noexcept { return m_lastError.lastError(); }
+	/// @brief Return the last error class (from git_last_error())
+	static auto lastClass() noexcept { return m_lastError.get<0>(); }
+	/// @brief Return the last error number (returned from git_* functions)
+	static auto lastErrno() noexcept { return m_lastError.get<1>(); }
 
 	GitTy *repo() const noexcept { return m_repo.get(); }
 	operator GitTy *() const noexcept { return repo(); }
 private:
 	explicit Repo(GitTy *repo) noexcept : m_repo(repo) {}
 
-	Holder m_repo;
-
+	friend class Diff;
 	friend class Index;
 	friend class PathSpec;
+	friend class Remote;
+	friend class RevWalk;
+	friend class Signature;
 	friend class Tag;
+	friend class Tree;
+	friend class TreeBuilder;
+
+	static std::pair<std::string, int> lastGitError() noexcept;
+
+	static int setLastError(int ret) {
+		if (ret) {
+			auto le = lastGitError();
+			m_lastError.reset().setError(std::move(le.first));
+			m_lastError.set<0>(le.second);
+			m_lastError.set<1>(ret);
+		}
+
+		return ret;
+	}
 
 	template<class Class, typename FunTy, typename... Args>
 	static std::optional<Class> MakeGit(const FunTy &fun, Args&&... args)
 	{
 		typename Class::GitTy *gitEntry;
-		if (fun(&gitEntry, std::forward<Args>(args)...))
+		if (setLastError(fun(&gitEntry, std::forward<Args>(args)...)))
 			return std::nullopt;
 		return Class(gitEntry);
 	}
@@ -170,13 +193,16 @@ private:
 	static std::optional<Class> MakeGitRepo(const Repo &repo, const FunTy &fun, Args&&... args)
 	{
 		typename Class::GitTy *gitEntry;
-		if (fun(&gitEntry, std::forward<Args>(args)...))
+		if (setLastError(fun(&gitEntry, std::forward<Args>(args)...)))
 			return std::nullopt;
 		return Class(repo, gitEntry);
 	}
 
 	static void checkoutProgress(const char *path, size_t completed_steps, size_t total_steps,
 				     void *payload);
+
+	Holder m_repo;
+	static thread_local SlHelpers::LastErrorStr<int, int> m_lastError;
 };
 
 }

include/git/Tree.h
@@ -13,12 +13,12 @@
 
 #include "Helpers.h"
 #include "Object.h"
+#include "Repo.h"
 
 namespace SlGit {
 
 class Blob;
 class Commit;
-class Repo;
 class TreeEntry;
 
 class Tree : public TypedObject<git_tree> {
@@ -33,7 +33,7 @@ public:
 
 	size_t entryCount() const noexcept { return git_tree_entrycount(tree()); }
 
-	int walk(const WalkCallback &CB, const git_treewalk_mode &mode = GIT_TREEWALK_PRE) const;
+	bool walk(const WalkCallback &CB, const git_treewalk_mode &mode = GIT_TREEWALK_PRE) const;
 
 	std::optional<TreeEntry> treeEntryByPath(const std::string &path) const noexcept;
 	TreeEntry treeEntryByIndex(size_t idx) const noexcept;
@@ -56,11 +56,13 @@ class TreeBuilder {
 public:
 	TreeBuilder() = delete;
 
-	int insert(const std::filesystem::path &file, const Blob &blob) const noexcept;
-	int remove(const std::filesystem::path &file) const noexcept {
-		return git_treebuilder_remove(treeBuilder(), file.c_str());
+	bool insert(const std::filesystem::path &file, const Blob &blob) const noexcept;
+	bool remove(const std::filesystem::path &file) const noexcept {
+		return !Repo::setLastError(git_treebuilder_remove(treeBuilder(), file.c_str()));
 	}
-	int clear() const noexcept { return git_treebuilder_clear(treeBuilder()); }
+	bool clear() const noexcept {
+		return !Repo::setLastError(git_treebuilder_clear(treeBuilder()));
+	}
 
 	std::optional<Tree> write(const Repo &repo) const noexcept;
 

src/git/Diff.cpp
@@ -3,6 +3,7 @@
 #include <git2.h>
 
 #include "git/Diff.h"
+#include "git/Repo.h"
 
 using namespace SlGit;
 
@@ -14,11 +15,11 @@ void SlHelpers::Deleter<git_diff>::operator()(git_diff *idx) const
 
 int Diff::forEach(const ForEachCB &forEachCB) const
 {
-	return git_diff_foreach(diff(), forEachCB.file ? fileCB : nullptr,
-				forEachCB.binary ? binaryCB : nullptr,
-				forEachCB.hunk ? hunkCB : nullptr,
-				forEachCB.line ? lineCB : nullptr,
-				const_cast<void *>(static_cast<const void *>(&forEachCB)));
+	return Repo::setLastError(git_diff_foreach(diff(), forEachCB.file ? fileCB : nullptr,
+						   forEachCB.binary ? binaryCB : nullptr,
+						   forEachCB.hunk ? hunkCB : nullptr,
+						   forEachCB.line ? lineCB : nullptr,
+						   const_cast<void *>(static_cast<const void *>(&forEachCB))));
 }
 
 int Diff::print(const git_diff_format_t &format, const PrintCB &printCB) const

commit 79cd08e80332f7c2a76cb898f7c864276473d5fe

git/Repo: fix bool/int confusion in update()
Remote::fetchRefspecs() was omitted to flip.

Fixes: 67160afa1f06 ("git: convert to LastError")

src/git/Repo.cpp
@@ -54,7 +54,7 @@ bool Repo::update(const std::filesystem::path &path, const std::string &remote)
 	if (!remoteOpt)
 		return false;
 
-	if (remoteOpt->fetchRefspecs())
+	if (!remoteOpt->fetchRefspecs())
 		return false;
 
 	const auto stats = remoteOpt->stats();