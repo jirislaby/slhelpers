7fa36725baf950b5302d03786dad632bbd32ed3d
Jiri Slaby <jirislaby@gmail.com>
2026-05-12 11:06:24 +0200

    PyBindings: initial support (kerncvs/SupportedConf)

---
+++ src/pybind/kerncvs.cpp
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include "kerncvs/SupportedConf.h"
+
+namespace py = pybind11;
+using namespace SlKernCVS;
+
+PYBIND11_MODULE(slkerncvs, m)
+{
+    m.doc() = "SlKernCVS – supported.conf parser";
+
+    py::class_<SupportedConf> cls(m, "SupportedConf");
+
+    py::enum_<SupportedConf::SupportState>(cls, "SupportState")
+        .value("NonPresent",           SupportedConf::SupportState::NonPresent)
+        .value("Unsupported",          SupportedConf::SupportState::Unsupported)
+        .value("UnsupportedOptional",  SupportedConf::SupportState::UnsupportedOptional)
+        .value("Unspecified",          SupportedConf::SupportState::Unspecified)
+        .value("Supported",            SupportedConf::SupportState::Supported)
+        .value("BaseSupported",        SupportedConf::SupportState::BaseSupported)
+        .value("ExternallySupported",  SupportedConf::SupportState::ExternallySupported)
+        .value("KMPSupported",         SupportedConf::SupportState::KMPSupported)
+        .export_values();
+
+    cls
+	    .def(py::init([](const std::string &conf) {
+			  return SupportedConf(conf);
+			  }),
+		 py::arg("conf"),
+		 "Parses supported.conf")
+	    .def("support_state",
+		 &SupportedConf::supportState,
+		 py::arg("module"),
+		 "Find supported state of module")
+	    .def("__repr__", [](const SupportedConf &) {
+		 return "<SupportedConf>";
+		 });
+}
