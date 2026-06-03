86b23bb3b2dd551e41911c050880982f6f709b35
Jiri Slaby <jirislaby@gmail.com>
2026-06-03 10:58:45 +0200

    helpers: add Enum

---
+++ include/helpers/Enum.h
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#pragma once
+
+#include <type_traits>
+
+namespace SlHelpers {
+
+/// @brief Helper class to iterate over enum values from 0 to Enum::Count - 1
+template <typename Enum, std::underlying_type_t<Enum> first = 0>
+class EnumRange {
+public:
+	/// @brief Type of the underlying enum values
+	using Underlying = std::underlying_type_t<Enum>;
+
+	/// @brief Iterator class to iterate over enum values
+        struct iterator {
+		/// @brief Current value of the iterator
+                Underlying v;
+
+		/// @brief Dereference operator to get the current enum value
+                Enum operator*() const { return static_cast<Enum>(v); }
+
+		/// @brief Pre-increment operator to move to the next enum value
+                iterator &operator++() {
+                        ++v;
+                        return *this;
+                }
+
+		/// @brief Equality operator to compare two iterators
+                bool operator==(const iterator &other) const { return v == other.v; }
+		/// @brief Inequality operator to compare two iterators
+                bool operator!=(const iterator &other) const { return v != other.v; }
+        };
+
+	/// @brief Returns an iterator to the beginning of the enum range
+        iterator begin() const { return { first }; }
+	/// @brief Returns an iterator to the end of the enum range
+        iterator end() const { return { static_cast<Underlying>(Enum::Count) }; }
+};
+
+}
